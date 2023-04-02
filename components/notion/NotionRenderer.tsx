ee5a887061f63796b3499e9054cf26d5c20712c3
Samuel Rowe <samuelrowe1999@gmail.com>
2023-04-02 22:48:17 +0530

    feat: updated notion renderer to support lists

--- components/notion/NotionRenderer.tsx
+++ components/notion/NotionRenderer.tsx
@@ -9,6 +9,14 @@ import { CodeBlock, ICodeBlockProps } from "./CodeBlock";
 import { Heading1Block, IHeading1BlockProps } from "./Heading1Block";
 import { Heading2Block, IHeading2BlockProps } from "./Heading2Block";
 import { Heading3Block, IHeading3BlockProps } from "./Heading3Block";
+import {
+  INumberedListItemBlockProps,
+  NumberedListItemBlock,
+} from "./NumberedListItemBlock";
+import {
+  BulletedListItemBlock,
+  IBulletedListItemBlockProps,
+} from "./BulletedListItemBlock";
 
 const Root = styled("div")``;
 
@@ -23,18 +31,29 @@ const components: Record<
   | FunctionComponent<IHeading1BlockProps>
   | FunctionComponent<IHeading2BlockProps>
   | FunctionComponent<IHeading3BlockProps>
+  | FunctionComponent<INumberedListItemBlockProps>
+  | FunctionComponent<IBulletedListItemBlockProps>
 > = {
   paragraph: ParagraphBlock,
   code: CodeBlock,
   heading_1: Heading1Block,
   heading_2: Heading2Block,
   heading_3: Heading3Block,
+  numbered_list_item: NumberedListItemBlock,
+  bulleted_list_item: BulletedListItemBlock,
 };
+
+interface IGroup {
+  type: string;
+  items: BlockObjectResponse[];
+}
 
 export const NotionRenderer: FunctionComponent<INotionRendererProps> = (
   props: INotionRendererProps
 ): ReactElement => {
   const { page } = props;
+
+  // console.log(page);
 
   const renderBlock = (block: BlockObjectResponse) => {
     const Component = components[block.type];
@@ -42,8 +61,53 @@ export const NotionRenderer: FunctionComponent<INotionRendererProps> = (
       console.warn(`Unknown block type "${block.type} -- ignored"`);
       return <Fragment key={block.id} />;
     }
+    // console.log(block);
     return <Component key={block.id} block={block as never} />;
   };
 
-  return <Root>{page.results.map(renderBlock as any)}</Root>;
+  const renderGroups = (blocks: BlockObjectResponse[]) => {
+    /* Determine blocks that need to be grouped under a single parent. */
+    const groups: IGroup[] = [];
+    for (let i = 0; i < blocks.length; i++) {
+      const block = blocks[i];
+      if (["numbered_list_item", "bulleted_list_item"].includes(block.type)) {
+        if (
+          groups.length > 0 &&
+          groups[groups.length - 1].type === block.type
+        ) {
+          groups[groups.length - 1].items.push(block);
+          continue;
+        }
+        groups.push({
+          type: block.type,
+          items: [block],
+        });
+      } else {
+        groups.push({
+          type: block.type,
+          items: [block],
+        });
+      }
+    }
+
+    return (
+      <>
+        {groups.map((group, index) => (
+          <Fragment key={index}>
+            {group.type === "numbered_list_item" && (
+              <ol>{group.items.map(renderBlock)}</ol>
+            )}
+            {group.type === "bulleted_list_item" && (
+              <ul>{group.items.map(renderBlock)}</ul>
+            )}
+            {!["numbered_list_item", "bulleted_list_item"].includes(
+              group.type
+            ) && group.items.map(renderBlock)}
+          </Fragment>
+        ))}
+      </>
+    );
+  };
+
+  return <Root>{renderGroups(page.results as any)}</Root>;
 };
