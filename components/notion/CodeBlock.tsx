8a4ed50e78a7f30702c2941e969cf101fd2b8207
Samuel Rowe <samuelrowe1999@gmail.com>
2023-06-07 09:20:17 +0530

    feat: backup

--- components/notion/CodeBlock.tsx
+++ components/notion/CodeBlock.tsx
@@ -1,9 +1,10 @@
-import CodeMirror from "@uiw/react-codemirror";
 import { FunctionComponent, ReactElement, useMemo } from "react";
-import { python } from "@codemirror/lang-python";
+// import { python } from "@codemirror/lang-python";
 import { CodeBlockObjectResponse } from "@notionhq/client/build/src/api-endpoints";
 import { styled } from "@mui/material";
 import "./CodeBlock.module.css";
+import { CodeEditor } from "../code-editor";
+import { useWindowDimensions } from "../../hooks";
 
 const Root = styled("div")`
   margin-top: ${({ theme }) => theme.spacing(2)};
@@ -22,13 +23,17 @@ export const CodeBlock: FunctionComponent<ICodeBlockProps> = (
     () => block.code.rich_text.map((text) => text.plain_text).join("\n"),
     [block.code.rich_text]
   );
+  const { height } = useWindowDimensions();
+
   return (
     <Root>
-      <CodeMirror
-        value={code}
-        extensions={[python()]}
-        theme="dark"
-        readOnly={true}
+      <CodeEditor
+        data={code}
+        language={"yaml"}
+        onChange={() => undefined}
+        disabled={true}
+        lineWrapping={false}
+        height="fit-content"
       />
     </Root>
   );
---
+++ hooks/useWindowDimensions.ts
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height,
+  };
+}
+
+export function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState<any>({
+    width: 0,
+    height: 0,
+  });
+
+  useEffect(() => {
+    const windowDimensions0 = getWindowDimensions();
+    setWindowDimensions(windowDimensions0);
+  }, []);
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowDimensions;
+}
