9dc3d054905e2748ac634cafb35590f7dd74ee2e
Samuel Rowe <samuelrowe1999@gmail.com>
2023-06-21 22:01:20 +0530

    feat: created `createEnquiryRow` utility for interacting with Notion API

--- utils/notion.ts
+++ utils/notion.ts
@@ -116,3 +116,31 @@ export const getPage = async (rootPageId: string, slug: string) => {
     }));
   return { blocks, page: pageMetaBySlug[slug] };
 };
+
+export interface IEnquiry {
+  name: string;
+  jobTitle: string;
+  company: string;
+  email: string;
+  message: string;
+}
+
+export const createEnquiryRow = async (enquiry: IEnquiry): Promise<void> => {
+  try {
+    // Create a new row in the Notion database
+    const response = await notion.pages.create({
+      parent: { database_id: process.env.NOTION_DATABASE_ID! },
+      properties: {
+        Name: { title: [{ text: { content: enquiry.name } }] },
+        JobTitle: { rich_text: [{ text: { content: enquiry.jobTitle } }] },
+        Company: { rich_text: [{ text: { content: enquiry.company } }] },
+        Email: { rich_text: [{ text: { content: enquiry.email } }] },
+        Message: { rich_text: [{ text: { content: enquiry.message } }] },
+      },
+    });
+
+    console.log("Enquiry created:", response);
+  } catch (error) {
+    console.error("Error creating enquiry:", error);
+  }
+};
