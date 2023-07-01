7e3ef7eb94821adfdd3c9e33a0fca7e132c060f3
Samuel Rowe <samuelrowe1999@gmail.com>
2023-07-01 09:54:46 +0530

    feat: created `useRecaptcha` hook

---
+++ hooks/useRecaptcha.ts
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import { useVerifyRecaptcha } from "./useVerifyRecaptcha";
+import { useIpAddress } from "./useIpAddress";
+import { useExecuteRecaptcha } from "./useExecuteRecaptcha";
+
+/* TODO: Handle errors! */
+export const useRecaptcha = (siteKey: string, action: string) => {
+  const verifyRecaptchaMutation = useVerifyRecaptcha();
+
+  const ipAddressQuery = useIpAddress();
+  const executeRecaptchaMutation = useExecuteRecaptcha();
+
+  useEffect(() => {
+    if (!ipAddressQuery.data) {
+      return;
+    }
+
+    executeRecaptchaMutation.mutate(
+      {
+        siteKey,
+        action,
+      },
+      {
+        onSuccess: (responseToken: string) => {
+          /* Send the token to the server for verification. */
+          verifyRecaptchaMutation.mutate({
+            responseToken,
+            userIpAddress: ipAddressQuery.data,
+          });
+        },
+      }
+    );
+  }, [
+    siteKey,
+    action,
+    verifyRecaptchaMutation,
+    ipAddressQuery.data,
+    ipAddressQuery.isSuccess,
+    executeRecaptchaMutation,
+  ]);
+};
