577ee13e390d847b545f08d171379f88e70adb0a
Samuel Rowe <samuelrowe1999@gmail.com>
2023-07-04 09:43:39 +0530

    feat: updated `ParagraphBlock` to use `RichText`

--- components/notion/ParagraphBlock.tsx
+++ components/notion/ParagraphBlock.tsx
@@ -1,9 +1,7 @@
-import { LinkProps, Typography, styled } from "@mui/material";
-import {
-  ParagraphBlockObjectResponse,
-  RichTextItemResponse,
-} from "@notionhq/client/build/src/api-endpoints";
+import { Typography, styled } from "@mui/material";
+import { ParagraphBlockObjectResponse } from "@notionhq/client/build/src/api-endpoints";
 import { FunctionComponent, ReactElement } from "react";
+import { RichText } from "./RichText";
 
 const Root = styled(Typography)`
   font-size: 18px;
@@ -11,92 +9,20 @@ const Root = styled(Typography)`
   margin-top: ${({ theme }) => theme.spacing(1)};
 `;
 
-const Code = styled("code")``;
-
-const Span = styled("span")``;
-
-const StyledLink = styled("a")`
-  text-decoration: none;
-  color: ${({ theme }) => theme.palette.primary.light};
-
-  &:hover {
-    color: ${({ theme }) => theme.palette.primary.dark};
-    text-decoration: underline;
-  }
-`;
-
 export interface IParagraphBlockProps {
   block: ParagraphBlockObjectResponse;
 }
-
-const colorMapping = {
-  default: "black",
-  gray: "grey",
-  brown: "brown",
-  orange: "orange",
-  yellow: "yellow",
-  green: "green",
-  blue: "blue",
-  purple: "purple",
-  pink: "pink",
-  red: "red",
-  gray_background: "grey",
-  brown_background: "brown",
-  orange_background: "orange",
-  yellow_background: "yellow",
-  green_background: "green",
-  blue_background: "blue",
-  purple_background: "purple",
-  pink_background: "pink",
-  red_background: "red",
-};
-
-const DummyLink: FunctionComponent<LinkProps> = (
-  props: LinkProps
-): ReactElement => {
-  const { children } = props;
-
-  return <>{children}</>;
-};
 
 export const ParagraphBlock: FunctionComponent<IParagraphBlockProps> = (
   props: IParagraphBlockProps
 ): ReactElement => {
   const { block } = props;
 
-  const renderSpan = (richText: RichTextItemResponse, index: number) => {
-    const { bold, italic, strikethrough, underline, code, color } =
-      richText.annotations;
-
-    const Wrapper = richText.href ? StyledLink : DummyLink;
-    const Component = code ? Code : Span;
-
-    return (
-      <Wrapper key={index} href={richText.href ?? undefined}>
-        <Component
-          style={{
-            fontFamily: "Roboto Slab",
-            fontWeight: bold ? "bold" : "normal",
-            fontStyle: italic ? "italic" : "normal",
-            textDecoration: [
-              strikethrough ? "strikethrough" : "",
-              underline ? "underline" : "",
-            ].join(" "),
-            // color: color.endsWith("_background")
-            //   ? undefined
-            //   : color === "default" && richText.href
-            //   ? undefined
-            //   : colorMapping[color],
-            // backgroundColor: color.endsWith("_background")
-            //   ? colorMapping[color]
-            //   : undefined,
-          }}
-        >
-          {richText.plain_text}
-        </Component>
-      </Wrapper>
-    );
-  };
-
-  return <Root>{block.paragraph.rich_text.map(renderSpan)}</Root>;
+  return (
+    <Root>
+      {block.paragraph.rich_text.map((richText, index) => (
+        <RichText key={index} richText={richText} index={index} />
+      ))}
+    </Root>
+  );
 };
