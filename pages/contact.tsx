a8142f7663216786bf9cfaed79186048586f7d27
Samuel Rowe <samuelrowe1999@gmail.com>
2023-12-22 00:27:38 +0530

    feat: updated contact form layout

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -2,6 +2,8 @@ import React, { ReactElement } from "react";
 import { useFormik } from "formik";
 import {
   Button,
+  Card,
+  CardContent,
   CircularProgress,
   Container,
   TextField,
@@ -13,7 +15,12 @@ import { PrimaryLayout } from "../components/layouts";
 import { TNextPageWithLayout } from "../types";
 import Recaptcha from "../components/common/Recaptcha";
 
-const Root = styled(Container)``;
+const Root = styled(Container)`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
 
 const MessageContainer = styled("div")`
   display: flex;
@@ -36,18 +43,24 @@ const MessageDescription = styled(Typography)`
   margin-top: ${({ theme }) => theme.spacing(2)};
 `;
 
-const FormContainer = styled("div")`
+const StyledCard = styled(Card)`
+  width: 400px;
+  border: 1px solid #dfdfdf;
+`;
+
+const StyledCardContent = styled(CardContent)`
   display: flex;
   flex-direction: column;
-  align-items: center;
+  align-items: flex-start;
+  padding: ${({ theme }) => theme.spacing(4)};
 `;
 
 const Form = styled("form")`
   display: flex;
   flex-direction: column;
   align-items: end;
-  width: 400px;
-  margin-top: ${({ theme }) => theme.spacing(2)};
+  width: 100%;
+  margin-top: ${({ theme }) => theme.spacing(3)};
   row-gap: ${({ theme }) => theme.spacing(2)};
 
   ${({ theme }) => theme.breakpoints.down("md")} {
@@ -58,6 +71,7 @@ const Form = styled("form")`
 const FormTitle = styled(Typography)`
   font-size: 20px;
   font-weight: 600;
+  text-align: left;
 `;
 
 const Submit = styled(Button)`
@@ -115,86 +129,90 @@ const EnquiryForm: TNextPageWithLayout = (): ReactElement => {
         </MessageContainer>
       )}
       {(createEnquiryMutation.isIdle || createEnquiryMutation.isLoading) && (
-        <FormContainer>
-          <FormTitle variant="h1">Contact us</FormTitle>
-          <Form onSubmit={formik.handleSubmit}>
-            <TextField
-              id="name"
-              name="name"
-              label="Name"
-              value={formik.values.name}
-              onChange={formik.handleChange}
-              error={formik.touched.name && Boolean(formik.errors.name)}
-              helperText={formik.touched.name && formik.errors.name}
-              fullWidth={true}
-              size="small"
-            />
+        <StyledCard elevation={0}>
+          <StyledCardContent>
+            <FormTitle variant="h1">Have questions? Let us know</FormTitle>
+            <Form onSubmit={formik.handleSubmit}>
+              <TextField
+                id="name"
+                name="name"
+                label="Name"
+                value={formik.values.name}
+                onChange={formik.handleChange}
+                error={formik.touched.name && Boolean(formik.errors.name)}
+                helperText={formik.touched.name && formik.errors.name}
+                fullWidth={true}
+                size="small"
+              />
 
-            <TextField
-              id="jobTitle"
-              name="jobTitle"
-              label="Job Title"
-              value={formik.values.jobTitle}
-              onChange={formik.handleChange}
-              error={formik.touched.jobTitle && Boolean(formik.errors.jobTitle)}
-              helperText={formik.touched.jobTitle && formik.errors.jobTitle}
-              fullWidth={true}
-              size="small"
-            />
+              <TextField
+                id="jobTitle"
+                name="jobTitle"
+                label="Job title"
+                value={formik.values.jobTitle}
+                onChange={formik.handleChange}
+                error={
+                  formik.touched.jobTitle && Boolean(formik.errors.jobTitle)
+                }
+                helperText={formik.touched.jobTitle && formik.errors.jobTitle}
+                fullWidth={true}
+                size="small"
+              />
 
-            <TextField
-              id="company"
-              name="company"
-              label="Company Name"
-              value={formik.values.company}
-              onChange={formik.handleChange}
-              error={formik.touched.company && Boolean(formik.errors.company)}
-              helperText={formik.touched.company && formik.errors.company}
-              fullWidth={true}
-              size="small"
-            />
+              <TextField
+                id="company"
+                name="company"
+                label="Company name"
+                value={formik.values.company}
+                onChange={formik.handleChange}
+                error={formik.touched.company && Boolean(formik.errors.company)}
+                helperText={formik.touched.company && formik.errors.company}
+                fullWidth={true}
+                size="small"
+              />
 
-            <TextField
-              id="email"
-              name="email"
-              label="Work Email"
-              value={formik.values.email}
-              onChange={formik.handleChange}
-              error={formik.touched.email && Boolean(formik.errors.email)}
-              helperText={formik.touched.email && formik.errors.email}
-              fullWidth={true}
-              size="small"
-            />
+              <TextField
+                id="email"
+                name="email"
+                label="Work email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                error={formik.touched.email && Boolean(formik.errors.email)}
+                helperText={formik.touched.email && formik.errors.email}
+                fullWidth={true}
+                size="small"
+              />
 
-            <TextField
-              id="message"
-              name="message"
-              label="Message"
-              value={formik.values.message}
-              onChange={formik.handleChange}
-              error={formik.touched.message && Boolean(formik.errors.message)}
-              helperText={formik.touched.message && formik.errors.message}
-              fullWidth={true}
-              multiline={true}
-              rows={4}
-              size="small"
-            />
+              <TextField
+                id="message"
+                name="message"
+                label="Message"
+                value={formik.values.message}
+                onChange={formik.handleChange}
+                error={formik.touched.message && Boolean(formik.errors.message)}
+                helperText={formik.touched.message && formik.errors.message}
+                fullWidth={true}
+                multiline={true}
+                rows={4}
+                size="small"
+              />
 
-            <Submit
-              type="submit"
-              variant="contained"
-              color="primary"
-              size="small"
-              disabled={createEnquiryMutation.isLoading}
-              disableElevation={true}
-            >
-              Submit{" "}
-              {createEnquiryMutation.isLoading && (
-                <CircularProgress size={14} sx={{ ml: 1 }} />
-              )}
-            </Submit>
-          </Form>
-        </FormContainer>
+              <Submit
+                type="submit"
+                variant="contained"
+                color="primary"
+                size="small"
+                disabled={createEnquiryMutation.isLoading}
+                disableElevation={true}
+              >
+                Submit{" "}
+                {createEnquiryMutation.isLoading && (
+                  <CircularProgress size={14} sx={{ ml: 1 }} />
+                )}
+              </Submit>
+            </Form>
+          </StyledCardContent>
+        </StyledCard>
       )}
     </Root>
   );
